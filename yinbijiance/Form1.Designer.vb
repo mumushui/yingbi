63e4c29f171ae9d33d9d13935aee4674d405f3f0
mumushui <13035857149@sina.cn>
2018-10-08 19:03:59 +0800

    firdt

---
+++ yinbijiance/Form1.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.HWindowControl1 = New HalconDotNet.HWindowControl()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(708, 153)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(152, 51)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "读取图片"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(708, 82)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(120, 21)
+        Me.TextBox1.TabIndex = 2
+        '
+        'HWindowControl1
+        '
+        Me.HWindowControl1.AutoSize = True
+        Me.HWindowControl1.BackColor = System.Drawing.Color.Black
+        Me.HWindowControl1.BorderColor = System.Drawing.Color.Black
+        Me.HWindowControl1.ImagePart = New System.Drawing.Rectangle(0, 0, 600, 600)
+        Me.HWindowControl1.Location = New System.Drawing.Point(24, 21)
+        Me.HWindowControl1.Name = "HWindowControl1"
+        Me.HWindowControl1.Size = New System.Drawing.Size(600, 600)
+        Me.HWindowControl1.TabIndex = 3
+        Me.HWindowControl1.WindowSize = New System.Drawing.Size(600, 600)
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(708, 237)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(152, 51)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "处理图片"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(708, 342)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(152, 51)
+        Me.Button3.TabIndex = 1
+        Me.Button3.Text = "相机初始化"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(708, 459)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(152, 51)
+        Me.Button4.TabIndex = 1
+        Me.Button4.Text = "采集显示"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 200
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(708, 550)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(152, 51)
+        Me.Button5.TabIndex = 1
+        Me.Button5.Text = "关闭相机"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(884, 761)
+        Me.Controls.Add(Me.HWindowControl1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents HWindowControl1 As HalconDotNet.HWindowControl
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+
+End Class
---
+++ yinbijiance/Form1.vb
@@ -0,0 +1,192 @@
+﻿Imports HalconDotNet
+
+Public Class Form1
+    Dim bln_Click As Int16 = 0
+   
+    ' Local iconic variables 
+    Dim ho_Th As HObject = Nothing, ho_GrayImage As HObject = Nothing
+    Dim ho_Regions As HObject = Nothing, ho_ConnectedRegions As HObject = Nothing
+    Dim ho_RegionClosing As HObject = Nothing, ho_RegionUnion1 As HObject = Nothing
+    Dim ho_RegionFillUp1 As HObject = Nothing, ho_RegionBorder As HObject = Nothing
+    Dim ho_Image As HObject = Nothing
+    Dim ho_RegionUnion As HObject = Nothing, ho_RegionFillUp As HObject = Nothing
+    Dim ho_RegionZoom As HObject = Nothing
+    Dim ho_ImageZoom As HObject = Nothing
+
+    ' Local control variables 
+    Dim hv_Area As HTuple = New HTuple, hv_Row1 As HTuple = New HTuple
+    Dim hv_Column1 As HTuple = New HTuple, hv_Width As HTuple = New HTuple
+    Dim hv_Height As HTuple = New HTuple, hv_Row As HTuple = New HTuple
+    Dim hv_Column As HTuple = New HTuple, hv_Radius As HTuple = New HTuple
+    Dim hv_Diameter As HTuple = New HTuple
+    Dim hv_AcqHandle As HTuple = New HTuple
+    Dim hv_k As HTuple = New HTuple, hv_WindowHandle1 As HTuple = New HTuple
+    ' Initialize local and output iconic variables 
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        bln_Click = 1
+
+        Dim ImagePath As String
+        OpenFileDialog1.Filter = "JPEG文件|*.jpg|BMP文件|*.bmp*"
+        OpenFileDialog1.RestoreDirectory = True
+        If (OpenFileDialog1.ShowDialog() = DialogResult.OK) Then
+
+            ImagePath = OpenFileDialog1.FileName
+            HOperatorSet.ReadImage(ho_Th, ImagePath)
+
+
+            HOperatorSet.GetImageSize(ho_Th, hv_Width, hv_Height)
+            Dim w As Double = hv_Width.TupleInt
+            Dim h As Double = hv_Height.TupleInt
+            If (hv_Width < 600 And hv_Height < 600) Then
+                HWindowControl1.Width = hv_Width
+                HWindowControl1.Height = hv_Height
+            ElseIf (hv_Width > 600 Or hv_Height > 600) Then
+                HWindowControl1.Width = 500
+                HWindowControl1.Height = HWindowControl1.Width * (h / w)
+
+
+            End If
+        End If
+        HOperatorSet.DispObj(ho_Th, HWindowControl1.HalconWindow)
+
+    End Sub
+
+
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+
+        If (bln_Click = 0) Then
+            MessageBox.Show("未选择图片")
+
+        ElseIf (bln_Click) Then
+            HOperatorSet.Rgb1ToGray(ho_Th, ho_GrayImage)
+            HOperatorSet.DispObj(ho_GrayImage, HWindowControl1.HalconWindow)
+            HOperatorSet.Threshold(ho_GrayImage, ho_Regions, New HTuple(0), New HTuple(210))
+
+            HOperatorSet.Connection(ho_Regions, ho_ConnectedRegions)
+
+            HOperatorSet.Closing(ho_ConnectedRegions, ho_ConnectedRegions, ho_RegionClosing _
+                )
+
+            HOperatorSet.Union1(ho_RegionClosing, ho_RegionUnion1)
+
+            HOperatorSet.FillUp(ho_RegionUnion1, ho_RegionFillUp1)
+
+            HOperatorSet.DispObj(ho_RegionFillUp1, HWindowControl1.HalconWindow)
+
+            HOperatorSet.AreaCenter(ho_RegionFillUp1, hv_Area, hv_Row1, hv_Column1)
+            HOperatorSet.GetImageSize(ho_GrayImage, hv_Width, hv_Height)
+
+            HOperatorSet.Boundary(ho_RegionFillUp1, ho_RegionBorder, New HTuple("inner"))
+            HOperatorSet.SmallestCircle(ho_RegionBorder, hv_Row, hv_Column, hv_Radius)
+            hv_Diameter = (New HTuple(2)).TupleMult(hv_Radius)
+            HOperatorSet.SetPart(HWindowControl1.HalconWindow, New HTuple(0), New HTuple(0), hv_Height, _
+          hv_Width)
+
+            HOperatorSet.DispObj(ho_RegionBorder, HWindowControl1.HalconWindow)
+            TextBox1.Text = hv_Diameter.ToString()
+
+            ho_Th.Dispose()
+            ho_GrayImage.Dispose()
+            ho_Regions.Dispose()
+            ho_ConnectedRegions.Dispose()
+            ho_RegionClosing.Dispose()
+            ho_RegionUnion1.Dispose()
+            ho_RegionFillUp1.Dispose()
+            ho_RegionBorder.Dispose()
+            bln_Click = 0
+        End If
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        bln_Click = 1
+        HOperatorSet.CloseAllFramegrabbers()
+        HOperatorSet.OpenFramegrabber(New HTuple("GigEVision"), New HTuple(0), New HTuple(0), _
+            New HTuple(0), New HTuple(0), New HTuple(0), New HTuple(0), New HTuple("default"), _
+            New HTuple(-1), New HTuple("default"), New HTuple(-1), New HTuple("false"), _
+            New HTuple("default"), New HTuple("0030531fa30d_Basler_ACA130MGED"), New HTuple(0), _
+            New HTuple(-1), hv_AcqHandle)
+
+
+    End Sub
+
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        HOperatorSet.GrabImageStart(hv_AcqHandle, New HTuple(-1))
+        HOperatorSet.GrabImageAsync(ho_Image, hv_AcqHandle, New HTuple(-1))
+        HOperatorSet.DispObj(ho_Image, HWindowControl1.HalconWindow)
+        HOperatorSet.GrabImageStart(hv_AcqHandle, New HTuple(-1))
+
+        HOperatorSet.GrabImageAsync(ho_Image, hv_AcqHandle, New HTuple(-1))
+        'Image Acquisition 01: Do something
+
+        HOperatorSet.Rgb1ToGray(ho_Image, ho_GrayImage)
+        HOperatorSet.GetImageSize(ho_GrayImage, hv_Width, hv_Height)
+
+        HOperatorSet.ZoomImageSize(ho_GrayImage, ho_ImageZoom, hv_Width, hv_Height, New HTuple("constant"))
+
+        HOperatorSet.Threshold(ho_GrayImage, ho_Regions, New HTuple(15), New HTuple(153))
+
+        HOperatorSet.Connection(ho_Regions, ho_ConnectedRegions)
+
+        HOperatorSet.Closing(ho_ConnectedRegions, ho_ConnectedRegions, ho_RegionClosing _
+            )
+
+        HOperatorSet.Union1(ho_RegionClosing, ho_RegionUnion)
+
+        HOperatorSet.FillUp(ho_RegionUnion, ho_RegionFillUp)
+        HOperatorSet.AreaCenter(ho_RegionFillUp, hv_Area, hv_Row, hv_Column)
+        hv_k = hv_Width.TupleDiv(New HTuple(600))
+        hv_Width = hv_Width.TupleDiv(hv_k)
+        hv_Height = hv_Height.TupleDiv(hv_k)
+        HOperatorSet.SetPart(HWindowControl1.HalconWindow, New HTuple(0), New HTuple(0), hv_Height, _
+            hv_Width)
+
+        HOperatorSet.ZoomRegion(ho_RegionFillUp, ho_RegionZoom, New HTuple(0.5), New HTuple(0.5))
+        dev_open_window_fit_size(New HTuple(0), New HTuple(0), hv_Area, hv_Area, New HTuple(-1), _
+            New HTuple(-1), hv_WindowHandle1)
+        HOperatorSet.DispObj(ho_RegionZoom, HWindowControl1.HalconWindow)
+        HOperatorSet.InnerCircle(ho_RegionZoom, hv_Row1, hv_Column1, hv_Radius)
+        hv_Diameter = hv_Radius.TupleMult(New HTuple(2))
+        TextBox1.Text = hv_Diameter.ToString()
+        '释放资源
+        ho_Image.Dispose()
+        ho_GrayImage.Dispose()
+        ho_ImageZoom.Dispose()
+        ho_Regions.Dispose()
+        ho_ConnectedRegions.Dispose()
+        ho_RegionClosing.Dispose()
+        ho_RegionUnion.Dispose()
+        ho_RegionFillUp.Dispose()
+        ho_RegionZoom.Dispose()
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        If (bln_Click = 0) Then
+            MessageBox.Show("相机未初始化或相机未连接")
+        ElseIf (bln_Click = 1) Then
+            Timer1.Enabled = True
+            HOperatorSet.GetImageSize(ho_Image, hv_Width, hv_Height)
+
+            ho_Image.Dispose()
+            bln_Click = bln_Click + 1
+        ElseIf (bln_Click > 1) Then
+            MessageBox.Show("请勿重复点击按钮")
+        End If
+       
+
+        bln_Click = 0
+
+    End Sub
+
+
+    Private Sub Form1_FormClosed(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosedEventArgs) Handles MyBase.FormClosed
+        HOperatorSet.CloseAllFramegrabbers()
+
+
+
+    End Sub
+End Class
